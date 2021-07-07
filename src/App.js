c6ff03abdc7c364e29b9d36eddeeda9c5149f986
Hariharen <huryharen@gmail.com>
2021-07-07 19:30:24 +0530

    added bootstrap adn r-b as packages and navbar

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import "./App.css";
 import Landing from "./Components/LandingPage/Landing";
 import Home from "./Components/Home/Home";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   return (
