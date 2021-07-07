3790cec4ae12e814303a340097c3e3987b107ca5
Hariharen <huryharen@gmail.com>
2021-07-07 19:48:08 +0530

    removed bootstrap navbar

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,15 +1,7 @@
 import React, { Component } from "react";
-import { Layout, Space } from "antd";
+import { Layout } from "antd";
 import { ReactComponent as ReactLogo } from "../../Assets/H_icon.svg";
 import { ReactComponent as Hamburger } from "../../Assets/hamburger.svg";
-import {
-  Button,
-  Nav,
-  Navbar,
-  NavDropdown,
-  Form,
-  FormControl,
-} from "react-bootstrap";
 
 import "./Home.css";
 
@@ -23,35 +15,9 @@ class Home extends Component {
           <Header>
             <div className="Logo-div">
               <ReactLogo />
-              <Hamburger />
             </div>
           </Header>
           <Content>
-            <Navbar bg="light" expand="lg">
-              <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
-              <Navbar.Toggle aria-controls="basic-navbar-nav" />
-              <Navbar.Collapse id="basic-navbar-nav">
-                <Nav className="mr-auto">
-                  <Nav.Link href="#home">Home</Nav.Link>
-                  <Nav.Link href="#link">Link</Nav.Link>
-                  <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-                    <NavDropdown.Item href="#action/3.1">
-                      Action
-                    </NavDropdown.Item>
-                    <NavDropdown.Item href="#action/3.2">
-                      Another action
-                    </NavDropdown.Item>
-                    <NavDropdown.Item href="#action/3.3">
-                      Something
-                    </NavDropdown.Item>
-                    <NavDropdown.Divider />
-                    <NavDropdown.Item href="#action/3.4">
-                      Separated link
-                    </NavDropdown.Item>
-                  </NavDropdown>
-                </Nav>
-              </Navbar.Collapse>
-            </Navbar>
             <h1 id="intro-text">Make your small business rstandout.</h1>
           </Content>
           <Footer>footer</Footer>
