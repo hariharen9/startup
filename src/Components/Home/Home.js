6c68fa5340a41ba5e6b7f99f4dc1822fbdcc9d1e
Hariharen <huryharen@gmail.com>
2021-07-07 19:17:44 +0530

    added bootstrap adn r-b as packages

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,29 +1,26 @@
 import React, { Component } from "react";
-import { Layout } from "antd";
+import { Layout, Space } from "antd";
 import { ReactComponent as ReactLogo } from "../../Assets/H_icon.svg";
+import { ReactComponent as Hamburger } from "../../Assets/hamburger.svg";
+
 import "./Home.css";
 
-const { Header, Footer, Sider, Content } = Layout;
+const { Header, Footer, Content } = Layout;
 
 class Home extends Component {
   render() {
     return (
       <div className="Home">
         <Layout>
-          <Header style={{ alignItems: "flex-start" }}>
+          <Header>
             <div className="Logo-div">
-              <ReactLogo
-                viewBox="0 0 200 200"
-                className="Logo"
-                id="Logo-header"
-              />
+              <ReactLogo />
+              <Hamburger />
             </div>
           </Header>
-          <Layout>
-            <Content>
-              <h1 id="intro-text">Make your small business standout.</h1>
-            </Content>
-          </Layout>
+          <Content>
+            <h1 id="intro-text">Make your small business rstandout.</h1>
+          </Content>
           <Footer>footer</Footer>
         </Layout>
       </div>
