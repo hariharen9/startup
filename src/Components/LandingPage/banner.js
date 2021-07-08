48deafe39a87a342369a40bc263f0a7ad4a2847e
Balaganesh <ssbalaganesh2001@gmail.com>
2021-07-08 23:32:51 +0530

    Changed the landing pages layout. NEED TO BE DONE: Add responsiveness to top-padding based on screen height (for the text HALCYON))

--- src/Components/LandingPage/banner.js
+++ src/Components/LandingPage/banner.js
@@ -1,21 +1,27 @@
 import "./banner.css";
 import banner from "./banner.mp4";
 import Home from "../Home/Home.js";
-
+import { withRouter } from "react-router-dom";
 function Banner() {
   return (
-    <div className="banner-page">
-      <div className="banner">
-        <video id="background-video" autoPlay loop muted>
-          <source src={banner} type="video/mp4" />
-        </video>
-        <h2 className="responsive">HALCYON</h2>
-      </div>
-      <h3 className="black font-weight-bold text-uppercase text-dark">
-        We make Websites, Mobile apps, Logos and Designs
-      </h3>
+    <div>
+      <div className="banner-page">
+        <div className="banner">
+          <video id="background-video" autoPlay loop muted>
+            <source src={banner} type="video/mp4" />
+          </video>
+          <h2 className="responsive">HALCYON</h2>
 
-      <button className="button-custom">View our website!</button>
+          <h3 className="sub-text">
+            We are Logo/Web/Mobile App developers in the Netherlands.
+          </h3>
+        </div>
+      </div>
+      <div className="button-div">
+        <button className="button-custom">
+          <h5>Continue to Website</h5>
+        </button>
+      </div>
     </div>
   );
 }
