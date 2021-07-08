e995da365025f938f2cdf4f02c93aff2dacdce30
Hariharen <huryharen@gmail.com>
2021-07-08 20:21:19 +0530

    changed teh padding error

--- src/Components/LandingPage/banner.js
+++ src/Components/LandingPage/banner.js
@@ -4,7 +4,7 @@ import Home from "../Home/Home.js";
 
 function Banner() {
   return (
-    <div className="banner-page ">
+    <div className="banner-page">
       <div className="banner">
         <video id="background-video" autoPlay loop muted>
           <source src={banner} type="video/mp4" />
@@ -14,7 +14,7 @@ function Banner() {
       <h3 className="black font-weight-bold text-uppercase text-dark">
         We make Websites, Mobile apps, Logos and Designs
       </h3>
-      <button>View our website!</button>
+      <button className="button-custom">View our website!</button>
     </div>
   );
 }
