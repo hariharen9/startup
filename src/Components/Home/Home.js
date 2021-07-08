d62648451f3e3c705ba18373059e10c1ee9bff45
Balaganesh <ssbalaganesh2001@gmail.com>
2021-07-08 10:17:08 +0530

    Added hamburger men, aligned header, added  a blank div, added span to text

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -2,8 +2,6 @@ import React, { Component } from "react";
 import { Layout } from "antd";
 
 import { ReactComponent as ReactLogo } from "../../Assets/H_icon.svg";
-import { ReactComponent as Hamburger } from "../../Assets/hamburger.svg";
-
 import "./Home.css";
 
 const { Header, Footer, Content } = Layout;
@@ -14,12 +12,50 @@ class Home extends Component {
       <div className="Home">
         <Layout>
           <Header>
-            <div className="Logo-div">
-              <ReactLogo />
+            <div class="float-container">
+              <div class="float-child-left">
+                <div className="Logo-div">
+                  <nav role="navigation">
+                    <div id="menuToggle">
+                      <input type="checkbox" />
+
+                      <span></span>
+                      <span></span>
+                      <span></span>
+
+                      <ul id="menu">
+                        <a href="#">
+                          <li>Home</li>
+                        </a>
+                        <a href="#">
+                          <li>Portfolio</li>
+                        </a>
+                        <a href="#">
+                          <li>About</li>
+                        </a>
+                        <a href="#">
+                          <li>Contact Us</li>
+                        </a>
+                      </ul>
+                    </div>
+                  </nav>
+                </div>
+              </div>
+              <div class="float-child-right">
+                <h2 className="companyName">Halcyon</h2>
+              </div>
             </div>
           </Header>
           <Content>
-            <h1 id="intro-text">Make your small business rstandout.</h1>
+            <div className="blankSpace"></div>
+            <h1 className="intro-text">
+              <div class="intro-text-div">
+                <span data-index="1">
+                  Make your small business standout<br></br>
+                </span>
+                <span data-index="2">in a crowd of competitors.</span>
+              </div>
+            </h1>
           </Content>
           <Footer>footer</Footer>
         </Layout>
