9a23b6635b34aa39f1575c746a83f9c62c2ef64e
Balaganesh <ssbalaganesh2001@gmail.com>
2021-07-08 18:18:47 +0530

    Changed color of about

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -2,10 +2,10 @@ import React from "react";
 import "./About.css";
 export default function About() {
   return (
-    <div class="row">
+    <div class="row" id="row-about">
       <div class="col">
         <h2 className="about-text">About Halcyon</h2>
-        <p className="about-content" align="left">
+        <p className="about-content" align="left" color="white">
           At vero eos et accusamus et iusto odio dignissimos ducimus qui
           blanditiis praesentium voluptatum deleniti atque corrupti quos dolores
           et quas molestias excepturi sint occaecati cupiditate non provident,
