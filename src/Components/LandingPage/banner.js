f2b449a7e067bb21265f9e9abf450b4b1c8eb18a
Hariharen <huryharen@gmail.com>
2021-07-08 18:33:29 +0530

    added landing page button

--- src/Components/LandingPage/banner.js
+++ src/Components/LandingPage/banner.js
@@ -1,13 +1,18 @@
 import "./banner.css";
 import banner from "./banner.mp4";
+import Home from "../Home/Home.js";
 
 function Banner() {
   return (
-    <div className="banner">
-      <video id="baclground-video" autoPlay loop muted>
-        <source src={banner} type="video/mp4" />
-      </video>
-      <h2>HALCYON</h2>
+    <div className="banner-page ">
+      <div className="banner">
+        <video id="background-video" autoPlay loop muted>
+          <source src={banner} type="video/mp4" />
+        </video>
+        <h2>HALCYON</h2>
+        <h3>We make Websites, Mobile apps, Logos and Designs</h3>
+      </div>
+      <button>View our website!</button>
     </div>
   );
 }
