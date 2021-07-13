1f8005f59a66031837535ac1d4198ac5b41b8803
Balaganesh <ssbalaganesh2001@gmail.com>
2021-07-13 10:30:17 +0530

    Changed mail chimp to group account

--- src/Components/Mailchimp/Mailchimp.js
+++ src/Components/Mailchimp/Mailchimp.js
@@ -16,15 +16,6 @@ export default function Mailchimp() {
           <h2 id="Form-text">How can we reach you?</h2>
           <div class="mc-field-group">
             <input
-              type="text"
-              name="NAME"
-              id="mce-NAME"
-              autocomplete="off"
-              placeholder="NAME"
-            />
-          </div>
-          <div class="mc-field-group">
-            <input
               type="email"
               name="EMAIL"
               id="mce-EMAIL"
@@ -35,6 +26,15 @@ export default function Mailchimp() {
           <div class="mc-field-group">
             <input
               type="text"
+              name="NAME"
+              id="mce-NAME"
+              autocomplete="off"
+              placeholder="NAME"
+            />
+          </div>
+          <div class="mc-field-group">
+            <input
+              type="number"
               name="PHONE"
               id="mce-PHONE"
               autocomplete="off"
@@ -50,37 +50,37 @@ export default function Mailchimp() {
                   className="check-input"
                   type="checkbox"
                   value="1"
-                  name="group[54133][1]"
-                  id="mce-group[54133]-54133-0"
+                  name="group[22962][1]"
+                  id="mce-group[22962]-22962-0"
                 />
-                <label for="mce-group[54133]-54133-0">&nbsp; Websites</label>
+                <label for="mce-group[22962]-22962-0">&nbsp; Websites</label>
               </li>
               <li>
                 <input
                   type="checkbox"
                   value="2"
-                  name="group[54133][2]"
-                  id="mce-group[54133]-54133-1"
+                  name="group[22962][2]"
+                  id="mce-group[22962]-22962-1"
                 />
-                <label for="mce-group[54133]-54133-1">&nbsp; Logos</label>
+                <label for="mce-group[22962]-22962-1">&nbsp; Logos</label>
               </li>
               <li>
                 <input
                   type="checkbox"
                   value="4"
-                  name="group[54133][4]"
-                  id="mce-group[54133]-54133-2"
+                  name="group[22962][4]"
+                  id="mce-group[22962]-22962-2"
                 />
-                <label for="mce-group[54133]-54133-2">&nbsp; Mobile Apps</label>
+                <label for="mce-group[22962]-22962-2">&nbsp; Mobile Apps</label>
               </li>
               <li>
                 <input
                   type="checkbox"
                   value="8"
-                  name="group[54133][8]"
-                  id="mce-group[54133]-54133-3"
+                  name="group[22962][8]"
+                  id="mce-group[22962]-22962-3"
                 />
-                <label for="mce-group[54133]-54133-3">
+                <label for="mce-group[22962]-22962-3">
                   &nbsp; UI/UX Designs
                 </label>
               </li>
@@ -96,7 +96,7 @@ export default function Mailchimp() {
           >
             <input
               type="text"
-              name="b_df8117da02e8d9a41e6f8c9ec_e6e7e423d8"
+              name="b_c56af95614ccaa17ba565f065_f834765253"
               tabindex="-1"
               value=""
             />
@@ -116,52 +116,3 @@ export default function Mailchimp() {
     </div>
   );
 }
-
-
-
-
-
-<!-- Begin Mailchimp Signup Form -->
-<link href="//cdn-images.mailchimp.com/embedcode/classic-10_7.css" rel="stylesheet" type="text/css">
-<style type="text/css">
-	#mc_embed_signup{background:#fff; clear:left; font:14px Helvetica,Arial,sans-serif; }
-	/* Add your own Mailchimp form style overrides in your site stylesheet or in this style block.
-	   We recommend moving this block and the preceding CSS link to the HEAD of your HTML file. */
-</style>
-<div id="mc_embed_signup">
-<form action="https://gmail.us6.list-manage.com/subscribe/post?u=c56af95614ccaa17ba565f065&amp;id=f834765253" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
-    <div id="mc_embed_signup_scroll">
-	<h2>Subscribe</h2>
-<div class="indicates-required"><span class="asterisk">*</span> indicates required</div>
-<div class="mc-field-group">
-	<label for="mce-EMAIL">Email  <span class="asterisk">*</span>
-</label>
-	<input type="email" value="" name="EMAIL" class="required email" id="mce-EMAIL">
-</div>
-<div class="mc-field-group">
-	<label for="mce-NAME">Name </label>
-	<input type="text" value="" name="NAME" class="" id="mce-NAME">
-</div>
-<div class="mc-field-group size1of2">
-	<label for="mce-PHONE">Phone Number </label>
-	<input type="number" name="PHONE" class="" value="" id="mce-PHONE">
-</div>
-<div class="mc-field-group input-group">
-    <strong>What can we do for you? </strong>
-    <ul><li><input type="checkbox" value="1" name="group[22962][1]" id="mce-group[22962]-22962-0"><label for="mce-group[22962]-22962-0">Websites</label></li>
-<li><input type="checkbox" value="2" name="group[22962][2]" id="mce-group[22962]-22962-1"><label for="mce-group[22962]-22962-1">Logos</label></li>
-<li><input type="checkbox" value="4" name="group[22962][4]" id="mce-group[22962]-22962-2"><label for="mce-group[22962]-22962-2">Mobile Apps</label></li>
-<li><input type="checkbox" value="8" name="group[22962][8]" id="mce-group[22962]-22962-3"><label for="mce-group[22962]-22962-3">UI/UX Designs</label></li>
-</ul>
-</div>
-	<div id="mce-responses" class="clear">
-		<div class="response" id="mce-error-response" style="display:none"></div>
-		<div class="response" id="mce-success-response" style="display:none"></div>
-	</div>    <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
-    <div style="position: absolute; left: -5000px;" aria-hidden="true"><input type="text" name="b_c56af95614ccaa17ba565f065_f834765253" tabindex="-1" value=""></div>
-    <div class="clear"><input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="button"></div>
-    </div>
-</form>
-</div>
-<script type='text/javascript' src='//s3.amazonaws.com/downloads.mailchimp.com/js/mc-validate.js'></script><script type='text/javascript'>(function($) {window.fnames = new Array(); window.ftypes = new Array();fnames[0]='EMAIL';ftypes[0]='email';fnames[1]='NAME';ftypes[1]='text';fnames[2]='PHONE';ftypes[2]='number';}(jQuery));var $mcj = jQuery.noConflict(true);</script>
-<!--End mc_embed_signup--></li>
